219777a5e5c8bca2ab2373b72d7694682e741091
neitiris <Veliantos@gmail.com>
2019-07-17 18:08:47 +0300

    Updates

---
+++ precache-manifest.deaf78a757d823e36f740ae18a5a952e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eed309459433ac143659de979e1eef1",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "0ef138d4a9daf27bf5f7",
+    "url": "/react_people-table/static/css/main.e21b8801.chunk.css"
+  },
+  {
+    "revision": "7e421af1556c7df75c45",
+    "url": "/react_people-table/static/js/2.158f0b66.chunk.js"
+  },
+  {
+    "revision": "0ef138d4a9daf27bf5f7",
+    "url": "/react_people-table/static/js/main.21e9974a.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
